4eb5f108655081170828c50592e4b18058be83fd
Uday Kyada <udaykyada@gmail.com>
2023-01-03 09:26:49 +0530

    letest pull

--- public/assest/js/script.js
+++ public/assest/js/script.js
@@ -32,3 +32,22 @@ $(function() {
     }
   });
 });
+
+// faq
+$(function() {
+  $('.inner_content').slideUp();
+  $('.title_tab.active').next().slideDown();
+  $('.title_tab').click(function(j) {   
+    if ($(this).hasClass('active')) {
+      $(this).removeClass('active');
+      $('.title_tab').removeClass('active');
+      $(this).next().slideUp();
+    }
+    else {
+      $('.title_tab').removeClass('active');
+      $('.inner_content').slideUp();
+      $(this).addClass('active');
+      $(this).next().slideDown();
+    }     
+  });
+});
